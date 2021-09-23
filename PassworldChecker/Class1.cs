17a8a491efbe9233c19785e9bc07bf56304a0360
Ivan Lidovskoi <90608498+DeLarge666@users.noreply.github.com>
2021-09-23 13:14:03 +0300

    Add files via upload

---
+++ PassworldChecker/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryPassword.Tests
+{
+    public class PasswordCheckerTest
+    {
+        public static bool Check_SpecSymbols_ReturnsFalse(string password)
+        {
+            return true;
+        }
+    }
+}
