78e3c4f3b54fe46d3f4a7872819c0cb28c102ba9
Ivan Lidovskoi <90608498+DeLarge666@users.noreply.github.com>
2021-09-28 09:27:16 +0300

    Update Source.cpp

--- C++/Source.cpp
+++ C++/Source.cpp
@@ -1,35 +1,39 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <string>
-#include  "opencv2/highgui/highgui.hpp"
-#include  "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+
 using namespace cv;
 using namespace std;
-Mat img;
 int main()
 {
-	// setlocale(LC_ALL, "Russian");
-	//char filename[80];//road.jpg
-	//cout << "Vvedite imya faila, v kotoriy hotite nesti izmeneniya, i najmite Ёнтер" << endl;
-	//cin.getline(filename, 80);
-	//cout << "Otkroy file";
-	//cout << filename << endl;
-
-	//Mat img = imread("road.jpg", 1);
+	/*int height = 520;
+	int widht = 840;
+	Mat img(height, widht, CV_8UC3);
+	Point textorg(100, img.rows / 2);
+	int frontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
+	double frontScale = 2;
+	Scalar color(200, 100, 50);
+	putText(img, "opencv step by step", textorg, frontFace, frontScale, color);
+	namedWindow("hello worold", 0);
+	imshow("hello world", img);
+	waitKey(0);
+	return 0;*/
 
 	int height = 520;
-	int width = 840;
-	Mat img(height, width, CV_8UC3);
-	Point textOrg(100, img.rows / 2);
-	int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
-	double fontScale = 2;
-	Scalar color(200, 100, 50);
-	putText(img, "OpenCV Step By Step", textOrg, fontFace, fontScale, color);
-	namedWindow("Hello world",0);
-	imshow("Hello world", img);
+	int widht = 840;
+	Mat img(height, widht, CV_8UC3);
+	Point textorg(100,img.rows/2);
+	int frontFace = FONT_HERSHEY_TRIPLEX;
+	double frontScale = 2;
+	Scalar color(0, 200, 21);
+	putText(img, "ZA IMPERATORA", textorg, frontFace, frontScale, color);
+	namedWindow("hello worold", 0);
+	imshow("hello world", img);
 	waitKey(0);
 	return 0;
 }
